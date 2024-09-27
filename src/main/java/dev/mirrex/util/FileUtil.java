23d39fc3fb246b4301f5f7fa07818475c7010070
Alex Belovitsky <mirrexone@gmail.com>
2024-09-27 21:50:20 +0300

    Update readFile method

--- src/main/java/dev/mirrex/util/FileUtil.java
+++ src/main/java/dev/mirrex/util/FileUtil.java
@@ -7,8 +7,11 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class FileUtil {
-    public static List<String> readFile(String filename) throws IOException {
-        Path path = Paths.get(filename);
+    public static List<String> readFile(String filePath) throws IOException {
+        Path path = Paths.get(filePath).toAbsolutePath().normalize();
+        if (!Files.exists(path)) {
+            throw new IOException("File not found: " + path);
+        }
         return Files.readAllLines(path);
     }
 }
